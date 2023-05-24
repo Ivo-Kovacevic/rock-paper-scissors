48a040bc545bcdbbe79f5edd721a98a13de66ad6
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2023-05-24 22:03:24 +0200

    UI
    Initial game UI.

--- javascript.js
+++ javascript.js
@@ -2,6 +2,13 @@ let playerSelection;
 let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
+let roundWinner;
+const buttons = document.querySelectorAll('button');
+const playerScoreText = document.querySelector('.playerScore');
+const computerScoreText = document.querySelector('.computerScore');
+const playerIcon = document.querySelector('.playerIcon');
+const computerIcon = document.querySelector('.computerIcon');
+const message = document.querySelector('.message');
 
 // Generating output for computer
 function getComputerChoice() {
@@ -18,43 +25,48 @@ function getComputerChoice() {
 
 // Compare player and computer choices and determine winner
 function playRound(playerSelection, computerSelection) {
-    let result;
-    if (playerSelection == computerSelection) {
-        result = "Tie";
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        result = "You lost! Paper beats rock.";
+    if (playerSelection === computerSelection) {
+        roundWinner = "tie";
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')
+      ) {
+        playerScore++;
+        roundWinner = 'player';
+    } else if (
+        (computerSelection === 'rock' && playerSelection === 'scissors') ||
+        (computerSelection === 'scissors' && playerSelection === 'paper') ||
+        (computerSelection === 'paper' && playerSelection === 'rock')
+      ) {
         computerScore++;
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        result = "You won! Rock beats scissors.";
-        playerScore++;
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        result = "You won! Paper beats rock.";
-        playerScore++;
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        result = "You lost! Scissors beat paper.";
-        computerScore++;
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        result = "You won! Scissors beat paper.";
-        playerScore++;
+        roundWinner = 'computer';
+    }
+}
+
+// Update display info after each pick
+function updateMessages() {
+    if (roundWinner === 'player') {
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        message.textContent = `You won! ${playerSelection} beats ${computerSelection}`;
+    } else if (roundWinner === 'computer') {
+        computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+        message.textContent = `You lost. ${computerSelection} beats ${playerSelection}`;
     } else {
-        result = "You lost! Rock beats scissors.";
-        computerScore++;
+        message.textContent = `Tie`;
     }
-    return result;
+    playerIcon.textContent = playerSelection.toUpperCase();
+    computerIcon.textContent = computerSelection.toUpperCase();
+    playerScoreText.textContent = playerScore;
+    computerScoreText.textContent = computerScore;
 }
 
-// Start game until someone wins 5 rounds
-function game() {
-    while (playerScore < 5 && computerScore < 5) {
-        playerSelection = prompt("Pick rock, paper or scissors.").toLowerCase();
-        while(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-            playerSelection = prompt("Invalid input! Pick rock, paper or scissors!").toLowerCase();
-        }
-        computerSelection = getComputerChoice();
-        console.log(`\nYou picked ${playerSelection}, and computer picked ${computerSelection}`);
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`You ${playerScore} - ${computerScore} Computer\n`);
-    }
+// Button click event
+function buttonClick() {
+    computerSelection = getComputerChoice();
+    playerSelection = this.classList[0];
+    playRound(playerSelection, computerSelection);
+    updateMessages();
 }
 
-game();
+buttons.forEach(button => button.addEventListener('click', buttonClick));
